7e96d4ace229b70dbc9e90f8b2a76f45645036f4
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2024-06-23 20:05:02 +0300

    feat: favicon, base styles, page structure
    Implements most of the design of the sidebar from the Figma file. The
    base project and tasks are imported from the `init.json` file.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 import './styles/reset.css';
 import './styles/style.css';
+import defaultProjects from './components/init.json';
+
+const domNav = document.querySelector('nav.project-navigation');
+
+let projects = JSON.parse(localStorage.getItem('@spaces-projects'));
+
+if(projects === null) {
+  projects = defaultProjects;
+}
+
+for(let [name, project] of Object.entries(projects)) {
+  const currentButton = document.createElement('div');
+  currentButton.classList.add('project-button');
+  currentButton.innerHTML = `
+    <div class="name-and-labels">
+      <p>@${name}</p>
+    </div>
+    <p>${project.tasks.length}</p>
+  `;
+  domNav.appendChild(currentButton);
+  currentButton.addEventListener('click', e => {
+    e.preventDefault();
+    currentButton.style.borderLeft = `4px solid hsl(${project.hue}deg 90% 60% / 100%)`;
+    currentButton.style.background = `hsl(${project.hue}deg 90% 60% / 5%)`;
+  });
+}
